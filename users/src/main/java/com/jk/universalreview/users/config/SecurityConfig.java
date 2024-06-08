97c7e060362b05d1185e27503f50af0e16c1f904
joyeshk <joyeshk@gmail.com>
2024-06-08 18:44:45 -0500

    Update SecurityConfig.java
    update spring security oauth config with latest methods

--- users/src/main/java/com/jk/universalreview/users/config/SecurityConfig.java
+++ users/src/main/java/com/jk/universalreview/users/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.jk.universalreview.users.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,14 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String[] whiteList = new String[]{"/actuator/*","/v3/api-docs/**","/swagger-ui/**","/users/signup"};
         http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/",
-                                "/actuator/*",
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**").permitAll()
+                        .requestMatchers(whiteList).permitAll()
                         .anyRequest().authenticated()
-                ).oauth2Client().and().oauth2ResourceServer().jwt();
-
+                ).oauth2Client(Customizer.withDefaults()).oauth2ResourceServer(spec->spec.jwt(Customizer.withDefaults()));
+        
         //allow all URLS
         //http.authorizeRequests().anyRequest().permitAll();
 
