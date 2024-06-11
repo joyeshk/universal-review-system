48b8dbc5a583eeb66b653508e4cebc280fc51ee4
joyeshk <joyeshk@gmail.com>
2024-06-10 20:12:24 -0500

    Entities microservice secured with OKTA

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # universal-review-system
+
+Microservices are protected with OKTA Oauth2.0 specification.
+
+### get access token
+
+- open http://localhost:8081/auth/login login with user configured in the people section of Okta application in developer.okta.com
+
+### Users Microservice
+
+Swagger: http://localhost:8081/swagger-ui/index.html
+
+### Entitites Microservice
+
+Swagger: http://localhost:8082/swagger-ui/index.html
---
+++ entities/src/main/java/com/jk/universalreview/entities/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.jk.universalreview.entities.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String[] whiteList = new String[]{"/actuator/*","/v3/api-docs/**","/swagger-ui/**"};
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers(whiteList).permitAll()
+                .anyRequest().authenticated()
+        ).oauth2ResourceServer(spec->spec.jwt(Customizer.withDefaults()));
+
+        //allow all URLS
+        //http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
+    }
+}
