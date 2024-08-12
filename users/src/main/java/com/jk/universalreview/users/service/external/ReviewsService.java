61f69e2c2d83bc16bafa5ee986ce069ec3476b69
Joyesh Karmakar <joyeshk@gmail.com>
2024-08-11 20:22:57 -0500

    spring retry implemented

--- users/src/main/java/com/jk/universalreview/users/service/external/ReviewsService.java
+++ users/src/main/java/com/jk/universalreview/users/service/external/ReviewsService.java
@@ -2,6 +2,8 @@ package com.jk.universalreview.users.service.external;
 
 import com.jk.universalreview.users.DTO.external.Reviews;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,5 +14,6 @@ import java.util.List;
 public interface ReviewsService {
 
     @RequestMapping("/reviews/getAllUserReviews/{user_id}")
+    @Retryable(retryFor = RuntimeException.class, maxAttempts = 3, backoff = @Backoff(delay = 3000))
     List<Reviews> getUserReviews(@PathVariable("user_id") String userId);
 }
